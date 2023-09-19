329a89e1d176621376f84b9ebcfdc48e28419066
Farkas Krisztian <farkaskrisz9898@gmail.com>
2023-09-19 12:52:08 +0200

    Javitott verzió

--- main.cpp
+++ main.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
-//FST CMT
+
 constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int* b = new int[N_ELEMENTS]; // NELEMENTS helyett N_ELEMENTS 
+    std::cout << "1-100 ertekek duplazasa" << std::endl; // Hiányzott az << std::endl, és a kettőspont hibás
+    for (int i = 0; i < N_ELEMENTS; i++) // Hiányzott a < N_ELEMENTS, i++ hibás
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i < N_ELEMENTS; i++) // Igaznak kell lennie, i++ hibás 
     {
-        std::cout << "Ertek:"
-    }    
+        std::cout << "Ertek: " << b[i] << std::endl; // A hibás szövegezést és a hiányzó << std::endl-t hozzáadtam
+    }
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag = 0; // A változónak inicializálva kell lennie
+    for (int i = 0; i < N_ELEMENTS; i++) // Pontosvesszőt kell használni és i++ hibás 
     {
-        atlag += b[i]
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
